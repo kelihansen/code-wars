3eb4e20f5920aed02512ceb69ad1170bd88e23f2
Keli Hansen <keli.p.hansen@gmail.com>
2018-03-01 21:27:07 -0800

    add linter and clean up problems

--- object-oriented-piracy.js
+++ object-oriented-piracy.js
@@ -19,14 +19,14 @@ function Ship(draft, crew) {
     this.draft = draft,
     this.crew = crew,
     this.draftPerCrewMember = 1.5,
-    this.crewDraft = this.crew * this.draftPerCrewMember,
-    this.bootyDraft = this.draft - this.crewDraft
-  };
-  
-  const pirateCaptain = {
+    this.crewDraft = this.crew * this.draftPerCrewMember;
+    this.bootyDraft = this.draft - this.crewDraft;
+}
+
+const pirateCaptain = {
     bootyDraftThreshold: 20
-  }
-  
-  Ship.prototype.isWorthIt = function() {
+};
+
+Ship.prototype.isWorthIt = function() {
     return this.bootyDraft > pirateCaptain.bootyDraftThreshold;
-  };
+};
